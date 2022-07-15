f34077e56e71c5b16140bba49fcccd72a56c7021
“crlnvls” <cveloso@hotmail.co.uk>
2022-07-15 12:46:14 +0100

    new data

--- server/app.js
+++ server/app.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const cors = require("cors");
+
+const data = require("./data");
 
 // Create an Express app
 const app = express();
 
-// get information from reception
+// Tell the app to listend to JSON bodies on POST requests
+app.use(express.json());
+
+// Add 'headers' to each response, saying that we're okay with sharing resources with others
+app.use(cors());
+
+// get = request information from reception
 // req = request
 // res = response
 // "/" = where the user goes
@@ -12,9 +21,58 @@ app.get("/", (req, res) => {
   res.send("Welcome to the Ice Cream API!");
 });
 
-// Actually starts the server
+// Actually starts the server --> move this to index.js to test them separetely
 // app.listen(3000, () => {
 //   console.log("Started listening on port 3000");
 // });
+
+app.get("/flavours", (req, res) => {
+  let flavours = data;
+
+  if (req.query.vegan === "true") {
+    flavours = flavours.filter((f) => f["vegan"]);
+  }
+
+  //console.log(req.query)
+
+  res.json({
+    flavours: flavours.map((f) => f["flavour"]),
+  });
+});
+
+app.get("/flavours/:id", (req, res) => {
+  // res.send(req.params.id);
+
+  //Extract the paramenter from the URL
+  const id = req.params.id;
+
+  // Filter the data for the ice cream with that id
+  const filteredData = data.filter((f) => f["id"] == id);
+
+  if (filteredData.length == 1) {
+    // Send the first filtered result
+    res.json({
+      flavour: filteredData[0],
+    });
+  } else {
+    res.status(404).json({
+      error: "No such ice cream",
+    });
+  }
+});
+
+// post = create new info
+app.post("/flavours", (req, res) => {
+  console.log(req.body);
+
+  const newFlavour = req.body;
+  newFlavour["id"] = data.length + 1;
+  data.push(newFlavour);
+
+  res.status(201).json({
+    success: true,
+    flavour: newFlavour,
+  });
+});
 
 module.exports = app;
--- server/test/app.test.js
+++ server/test/app.test.js
@@ -17,4 +17,29 @@ describe("API", () => {
   it("Responds to GET resquest at / with a 200 status", (done) => {
     request(api).get("/").expect(200, done);
   });
+
+  it("Responds to GET resquest at / flavours a 200 status", (done) => {
+    request(api).get("/flavours").expect(200, done);
+  });
+
+  it("Responds to GET resquest at / flavorous with a JSON object", (done) => {
+    request(api).get("/flavours").expect("Content-Type", /json/, done);
+  });
+
+  //   it("Responds to GET resquest at / flavorous with a JSON object that has a 'flavours' key", (done) => {
+  //     request(api)
+  //       .get("/flavours")
+  //       .end(async (err, res) => {
+  //         try {
+  //           expect.assertions(2);
+  //           //res.body = res.json
+  //           const data = res.body;
+  //           expect("flavours" in data);
+  //           expect(data["flavours"] instanceof Array);
+  //           done();
+  //         } catch (err) {
+  //           done(err);
+  //         }
+  //       });
+  //   });
 });
